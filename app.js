70103e1a7829a4bdb14f27cd8c90eeb29ce60352
Đăng Khoa Đinh <ddkhoa.learning@gmail.com>
2021-05-01 13:09:17 +0200

    fix typo

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 
 // config express app
 app.set("view engine", "ejs"); // express use ejs as view engine
-app.set("views", path.join(__dirname, "views")); // express use ejs files in "view" folder to render html
+app.set("views", path.join(__dirname, "views")); // express use ejs files in "views" folder to render html
 app.use('/', express.static("public")); // express serve static files (css, js...) in "public" folder
 // to include the stylesheet at public/css/style.css, we use  <link rel="stylesheet" href="/css/style.css">
 // to include the script at public/script/script.js, we use <script src="/script/script.js"></script>
@@ -18,6 +18,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/notfound', function (req, res) {
-    res.render("notfound"); // error, we don't have notfound.ejs find in views folder
-})
+    res.render("notfound"); // error, we don't have notfound.ejs file in views folder
+});
+
 app.listen(80, function () { console.log("Server is listening on port 80. Visit : http://localhost") });
